d827932f77d47a146fc4cc58814a8e80bffb3d75
Aleksei Prokopov <aleksei.prokopov@novater.com>
2021-02-06 20:14:37 +0200

    only my configuration

--- examples/UTFT_Demo_400x240/UTFT_Demo_400x240.ino
+++ examples/UTFT_Demo_400x240/UTFT_Demo_400x240.ino
@@ -15,9 +15,6 @@
 // Declare which fonts we will be using
 extern uint8_t SmallFont[];
 
-// Uncomment the next line for Arduino 2009/Uno
-//UTFT myGLCD(ITDB32WC,19,18,17,16);   // Remember to change the model parameter to suit your display module!
-
 // Uncomment the next line for Arduino Mega
 UTFT myGLCD(ITDB32WC,38,39,40,41);   // Remember to change the model parameter to suit your display module!
 
